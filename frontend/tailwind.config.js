4b87f189782c8f970e9ccb5227305a1120c2b8ef
Bach <bach.tlg03@gmail.com>
2025-06-26 15:26:00 -0400

    Updated HomePage and added CORS to backend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,9 +5,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    daisyui: {
-      themes: ["forest"],
-    },
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: ["forest"],
+  },
 };
