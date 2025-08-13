08e13a09323ef593e23b43b64f46a3bc5807b945
Bach <bach.tlg03@gmail.com>
2025-08-13 17:23:27 -0400

    Finish connecting AskPage to backent

--- frontend/src/pages/AskPage.jsx
+++ frontend/src/pages/AskPage.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import api from "../lib/axios";
 
 const AskPage = () => {
   const [isRateLimited, setRateLimited] = useState(false);
+  const [query, setQuery] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     try {
@@ -14,6 +18,22 @@ const AskPage = () => {
     }
   }, []);
 
+  const handleQuerySubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await api.post("/notes/ask", {
+        query: query,
+      });
+      setResponse(res.data.message);
+      setQuery("");
+    } catch (e) {
+      console.log(`Error in handling query submit ${e}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen">
       <Navbar />
@@ -21,11 +41,29 @@ const AskPage = () => {
       {!isRateLimited && (
         <div className="container mx-auto px-4">
           <div className="max-w-2xl mx-auto">
-            <input
-              type="text"
-              placeholder="Enter your question here..."
-              className="w-full p-4 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+            <form onSubmit={handleQuerySubmit}>
+              <input
+                type="text"
+                placeholder="Enter your question here..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className="w-full p-4 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-white-500"
+              />
+            </form>
+          </div>
+        </div>
+      )}
+      {loading && (
+        <div className="flex justify-center items-center mt-4">
+          <div className="w-8 h-8 border-4 border-gray-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}
+      {response && (
+        <div className="container mx-auto px-4">
+          <div className="max-w-5xl mx-auto">
+            <p className="w-full min-h-64 mt-4 p-4 shadow-sm whitespace-pre-wrap">
+              {response}
+            </p>
           </div>
         </div>
       )}
